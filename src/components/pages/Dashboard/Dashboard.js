fc29e05a0b4f5f222a273047f53bb87d32183233
Abid Nawaz <abidpart2ics@gmail.com>
2024-11-18 23:56:51 +0500

    some ui updates

---
+++ src/components/pages/Dashboard/Dashboard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Card, Col, Row, Typography } from "antd";
+import { Content, Footer } from "antd/es/layout/layout";
+import Chart from "../../Chart/Chart";
+import BottomTable from "../../Section/Table/BottomTable";
+import styles from "./style.module.scss";
+import { CREATED_BY } from "../../../utils/constants";
+import { Columns, socialMedia } from "../Content/content";
+
+const Dashboard = () => {
+    return (
+        <Content>
+            <div className={styles.contentSectionWrapper}>
+                <Row>
+                    <Col span={24}>
+                        <Chart />
+                    </Col>
+                </Row>
+                <Row gutter={10}>
+                    {Columns.map((item, index) => {
+                        return (
+                            <Col key={index} flex={item.flex} sm={24} md={24} lg={24}>
+                                {item.children}
+                            </Col>
+                        );
+                    })}
+                </Row>
+                <BottomTable />
+            </div>
+            <Card className={styles.footerCard}>
+                <Footer className={styles.footer}>
+                    <Typography>{CREATED_BY}</Typography>
+                    <div className={styles.socialIcons}>
+                        {socialMedia.map((social, ind) => {
+                            return <img key={ind} src={social.source} alt={social.alt} />;
+                        })}
+                    </div>
+                </Footer>
+            </Card>
+        </Content>
+    );
+};
+
+export default Dashboard;
--- src/components/pages/header/Header.js
+++ src/components/pages/header/Header.js
@@ -3,9 +3,37 @@ import { Header } from "antd/es/layout/layout";
 import { Avatar, Input } from "antd";
 import styles from "./style.module.scss";
 import { SearchOutlined } from "@ant-design/icons";
-import { CustomImg } from '../../../utils/images';
+import { CustomImg } from "../../../utils/images";
+import { DownOutlined, SettingOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+
 
 const CustomHeader = () => {
+
+  const items = [
+    {
+      key: "1",
+      label: "My Account",
+      disabled: true,
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "2",
+      label: "Dashboard",
+      extra: "⌘P",
+    },
+    {
+      key: "4",
+      label: "Settings",
+      extra: "⌘S",
+      onClick: (route) => {
+        console.log('clicked',route)
+      }
+    },
+  ];
+
   return (
     <Header className={styles.topHeader}>
       <div className={styles.header}>
@@ -18,7 +46,21 @@ const CustomHeader = () => {
       </div>
       <div className={styles.topAvatar}>
         <img src={CustomImg.BellIcon} alt="img" />
-        <Avatar size="large" src={CustomImg.HeaderAvatar} />
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <Space>
+              <Avatar size="large" src={CustomImg.HeaderAvatar} />
+            </Space>
+          </a>
+        </Dropdown>
       </div>
     </Header>
   );
